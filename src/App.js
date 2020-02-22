5fcf9d9601704a6d58434f1763128839cce7e7aa
unknown <khaled05dz@gmail.com>
2020-02-22 21:43:06 +0100

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './homepage.component'
+
+function App() {
+
+  return(
+    <div>
+    <h1>Hello World 77</h1>
+   <HomePage/>
+   </div>
+
+  )
+ 
+}
+
+export default App;
